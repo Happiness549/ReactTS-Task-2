f5586a201891c496fd7d7f019ea1409a2ba14f78
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-07-18 12:13:13 +0200

    {

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import {Routes, Route} from 'react-router'
+import { Home } from './pages/Home'
+import { Navbar } from './components/Navbar/Navbar'
 
 function App() {
   
 
   return (
     <>
+    <Navbar />
+    <Routes>
+      <Route index element={<Home />} />
+    
+    </Routes>
      
     </>
   )
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from '../../assets/logo.png'
+import styles from './Nabar.module.css'
+import { Text } from '../../Text/Text'
+
+
+export const Navbar = () => {
+  return (
+    <nav>
+        <div className={styles['navbar-container']}>
+            <img src={logo} alt="App Logo" className={styles['logo']} />
+            <Text variant={'h2'} className={styles['navbar-text']}> Links Vault </Text>
+
+
+        </div>
+    </nav>
+  )
+}
