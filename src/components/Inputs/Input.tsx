25b35c9dfc87af610ab4391ba82d3b90e47627c4
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-07-20 20:00:43 +0200

    Add input.tsx interface

--- src/components/Inputs/Input.tsx
+++ src/components/Inputs/Input.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { type ChangeEvent } from 'react'
 
 interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement>{
     label: string;
     type: string;
-    placeholder: string
+    placeholder: string;
+    value: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Input: React.FC<InputFieldProps> = ({label, type, placeholder}) => {
+export const Input: React.FC<InputFieldProps> = ({label, type, placeholder, value, onChange}) => {
   return (
     <div>
-        label={label}
-        type={type}
-        placeholder={placeholder}
+        <label>{label}</label>
+        <input  type={type}
+                placeholder={placeholder} 
+                value={value}
+                onChange={onChange}/>
+       
 
     </div>
   )
